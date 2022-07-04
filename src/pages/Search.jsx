f2bcd4ad6b3c764e36a078df7b6eb4b677014663
Crisna <crisnabto@gmail.com>
2022-07-04 16:18:57 -0300

    Resolução do requisito 5

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -2,11 +2,65 @@ import React from 'react';
 import Header from '../components/Header';
 
 class Search extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      artistInput: '',
+      buttonDisabled: true,
+    };
+  }
+
+  handleChange = ({ target }) => {
+    const { name } = target;
+    this.setState(() => ({
+      [name]: target.value,
+    }),
+    this.activateButton);
+  }
+
+  validateName = (input) => {
+    const min = 2;
+    if (input.length >= min) {
+      return true;
+    }
+    return false;
+  }
+
+  activateButton = () => {
+    const { artistInput } = this.state;
+    const validate = this.validateName(artistInput);
+    if (validate) {
+      this.setState(() => ({ buttonDisabled: false }));
+    } else {
+      this.setState(() => ({ buttonDisabled: true }));
+    }
+  }
+
   render() {
+    const { buttonDisabled } = this.state;
     return (
       <div data-testid="page-search">
         <Header />
-        Search
+        <form>
+          <label htmlFor="page-search">
+            <input
+              type="text"
+              name="artistInput"
+              data-testid="search-artist-input"
+              onChange={ this.handleChange }
+            />
+          </label>
+
+          <button
+            data-testid="search-artist-button"
+            type="button"
+            disabled={ buttonDisabled }
+            onClick={ this.activateButton }
+          >
+            Pesquisar
+          </button>
+        </form>
       </div>
     );
   }
