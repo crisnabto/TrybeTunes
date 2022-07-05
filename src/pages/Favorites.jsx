4252c27b448b1d15c3fa09962bd216012f47eec6
Crisna <crisnabto@gmail.com>
2022-07-05 02:09:57 -0300

    Resolução do requisito 13

--- src/pages/Favorites.jsx
+++ src/pages/Favorites.jsx
@@ -1,12 +1,54 @@
 import React from 'react';
 import Header from '../components/Header';
+import MusicCard from '../components/MusicCard';
+import { getFavoriteSongs } from '../services/favoriteSongsAPI';
+import Loading from './Loading';
 
 class Favorites extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      carregando: false,
+      favoriteSongs: [],
+      // favorite: false,
+    };
+  }
+
+  componentDidMount() {
+    this.getAllFavorites();
+  }
+
+  // componentDidUpdate(prevProps, prevState) {
+  //   if (this.state.favorite !== prevState.favorite) {
+  //     this.refreshComponent();
+  //   }
+  // }
+
+  // refreshComponent = () => {
+  //   this.getAllFavorites();
+  // }
+
+  getAllFavorites = async () => {
+    this.setState({ carregando: true });
+    const allFavoriteSongs = await getFavoriteSongs();
+    // this.setState({favoriteSongs: allFavoriteSongs})
+    this.setState({ carregando: false, favoriteSongs: allFavoriteSongs });
+  }
+
   render() {
+    const { carregando, favoriteSongs } = this.state;
     return (
       <div data-testid="page-favorites">
         <Header />
-        Favorites
+        { carregando ? <Loading /> : (
+
+          favoriteSongs.map((song, index) => (
+            <MusicCard songs={ song } key={ index } />
+          ))
+
+        )}
+
       </div>
     );
   }
--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,12 +1,48 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Header from '../components/Header';
+import { getUser } from '../services/userAPI';
+import Loading from './Loading';
 
 class Profile extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      carregando: false,
+      userObj: '',
+    };
+  }
+
+  componentDidMount() {
+    this.getUserInfo();
+  }
+
+  getUserInfo = async () => {
+    this.setState({ carregando: true });
+    const userInfo = await getUser();
+    this.setState({ carregando: false, userObj: userInfo });
+    console.log(userInfo);
+  }
+
   render() {
+    const { userObj, carregando } = this.state;
+    const { name, description, email, image } = userObj;
     return (
       <div data-testid="page-profile">
         <Header />
-        Profile
+        {carregando ? <Loading /> : (
+
+          <section>
+            <h3>{name}</h3>
+            <p>{email}</p>
+            <p>{description}</p>
+            <img data-testid="profile-image" src={ image } alt="name" />
+
+            <Link to="/profile/edit">Editar perfil</Link>
+          </section>
+
+        )}
       </div>
     );
   }
